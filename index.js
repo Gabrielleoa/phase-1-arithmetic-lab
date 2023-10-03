8796e0b48695e181850018a192647b963eb46139
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-03 10:18:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+const random = Math.floor(Math.random() *(100-1)+1)
+console.log(random);
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod) 
+
+const numbers = [5, 15, 19, 7, 12, 20];
+const max = Math.max(...numbers);
+console.log(max);
+
+
+  
+
+
